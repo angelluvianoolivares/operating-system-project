2a706d24762004e0cf850b5824bbfdb29ca1f3d1
rodomendoza98-code <rodomendoza98@gmail.com>
2025-12-07 19:37:53 -0600

    Added the preemtive algorithm
    Updated comments and added runSRTF method.

--- Scheduler.h
+++ Scheduler.h
@@ -5,32 +5,23 @@
 #include <iomanip>      // For formatting the output table
 #include <algorithm>    // For sorting the processes
 #include "Process.h"    // Our 'Process' class
-#include <string>       // Added this for string
+#include <string>       // Added this for string (3)
 
 class Scheduler {
 public:
     // -Constructor- (initializes our scheduler)
     Scheduler();
 
-    /* Instead of the Scheduler holding on to one list (which gets ruined when using an algorithm),
-    *  we make our run function accept a list of processes.)
-    void addProcess(Process p);
-    */
-
     //-Runs the FCFS simulation on a given list of processes-
     void runFCFS(std::vector<Process> processes);
 
-    //-Runs the SJF simulation on a given list of processes- (4)
+    //-Runs the SJF simulation on a given list of processes- (3)
     void runSJF(std::vector<Process> processes);
 
-private:
-    /*
-    //-A list to hold all the processes for the simulation-
-    std::vector<Process> processes;
+    //-Runs the SRTF simulation on a given list of processes- (3)
+    void runSRTF(std::vector<Process> processes); 
 
-    //-To keep track of simulation time we use a clock-
-    int currentTime;
-    */
+private:
 
     //-A helper method to print the list and tittle-
     void printResults(std::vector<Process>& processes, std::string algorithmName);
