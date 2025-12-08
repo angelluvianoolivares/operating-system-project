1c0d163e8bc4dd61e952d3726d2a0fa49ea76a83
rodomendoza98-code <rodomendoza98@gmail.com>
2025-12-07 18:58:12 -0600

    Added SJF

--- Scheduler.h
+++ Scheduler.h
@@ -5,25 +5,33 @@
 #include <iomanip>      // For formatting the output table
 #include <algorithm>    // For sorting the processes
 #include "Process.h"    // Our 'Process' class
+#include <string>       // Added this for string
 
 class Scheduler {
 public:
     // -Constructor- (initializes our scheduler)
     Scheduler();
 
-    //-Method to add a new process to our list-
+    /* Instead of the Scheduler holding on to one list (which gets ruined when using an algorithm),
+    *  we make our run function accept a list of processes.)
     void addProcess(Process p);
+    */
 
-    //-Runs the FCFS simulation-
-    void runFCFS();
+    //-Runs the FCFS simulation on a given list of processes-
+    void runFCFS(std::vector<Process> processes);
+
+    //-Runs the SJF simulation on a given list of processes- (4)
+    void runSJF(std::vector<Process> processes);
 
 private:
+    /*
     //-A list to hold all the processes for the simulation-
     std::vector<Process> processes;
 
     //-To keep track of simulation time we use a clock-
     int currentTime;
+    */
 
-    //-A helper method to print the results table-
-    void printResults();
+    //-A helper method to print the list and tittle-
+    void printResults(std::vector<Process>& processes, std::string algorithmName);
 };
