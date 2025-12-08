cfe764308d083aa39cd775f4eecbd1874c194eaa
rodomendoza98-code <rodomendoza98@gmail.com>
2025-12-07 18:58:48 -0600

    Added SJF

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "auth.h"
 #include "Process.h" 
 #include "Scheduler.h" //(3)
+#include <vector> //Needed to create a list (4)
 
 using namespace std; 
 
@@ -25,24 +26,29 @@ int main()
         cout << "\n===================================" << endl;
         cout << "--- Running Process Scheduler ---" << endl;
 
-        // 1. Create a Scheduler instance
+        // 1. Create ONE Scheduler instance
         Scheduler myScheduler;
 
-        // 2. Create the processes (this is our test input)
-        Process p1(1, 0, 5);
-        Process p2(2, 2, 3);
-        Process p3(3, 4, 1);
-        Process p4(4, 5, 4);
+        // 2. Create ONE "master list" of processes
+        std::vector<Process> myProcesses;
+        myProcesses.push_back(Process(1, 0, 8));
+        myProcesses.push_back(Process(2, 1, 4));
+        myProcesses.push_back(Process(3, 2, 9));
+        myProcesses.push_back(Process(4, 3, 5));
 
         // 3. Add processes to the scheduler
-        myScheduler.addProcess(p1);
-        myScheduler.addProcess(p2);
-        myScheduler.addProcess(p3);
-        myScheduler.addProcess(p4);
+        myProcesses.clear(); // Clear the list
+        myProcesses.push_back(Process(1, 2, 3));
+        myProcesses.push_back(Process(2, 4, 2));
+        myProcesses.push_back(Process(3, 5, 1));
+        myProcesses.push_back(Process(4, 7, 4));
+        myProcesses.push_back(Process(5, 9, 2));
+        myProcesses.push_back(Process(6, 15, 6));
+        myProcesses.push_back(Process(7, 16, 8));
 
-        // 4. Run the FCFS simulation and print results
-        myScheduler.runFCFS();
-        
+        // 4. Run the Algorithms and print results
+        myScheduler.runFCFS(myProcesses);
+        myScheduler.runSJF(myProcesses);
         cout << "===================================" << endl;
     }
 
